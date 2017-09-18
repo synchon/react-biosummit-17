672db3a4e31e21cf82fc5e63d4843fe6df2a5425
Synchon Mandal <synchonmandal@gmail.com>
2017-09-18 14:47:16 +0530

    Update Footer.js

--- src/Footer.js
+++ src/Footer.js
@@ -7,7 +7,7 @@ const Footer = props => {
                 <div className="content has-text-centered">
                     <p><strong>Registration Queries:</strong></p>
                     <p>Dr. S. Vino: <a href="tel:+919944807642">+91 9944807642</a></p>
-                    <p>Mr. Anurag: <a href="tel:+919629256306">+91 9629256306</a></p>
+                    <p>Ms. Shruthi Suthakaran: <a href="tel:+919884074153">+91 9884074153</a></p>
                     <p><a href="mailto:vitbiosummit%40gmail%2ecom?subject=Registration Query">vitbiosummit@gmail.com</a></p>
                     <br/>
                     <p>© {props.year} {props.author}</p>
